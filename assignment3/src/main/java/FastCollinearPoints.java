630c43e2d1dc9feb6d2bf29a11fac75369d5997e
Artem Gaidaichuk <gaidaichuka@booxware.de>
2018-10-23 17:35:00 +0200

    Add implementation for As.3 FastCollinearPoints.java Also add resources for testing of As.3 Add pom.xml for As.3

--- assignment3/src/main/java/FastCollinearPoints.java
+++ assignment3/src/main/java/FastCollinearPoints.java
@@ -1,30 +1,48 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class FastCollinearPoints {
-    private ArrayList<LineSegment> lineSegments;
+    private LinkedList<LineSegment> lineSegments;
+    private Map<Point, LinkedList<Double>> endpoints;
 
     public FastCollinearPoints(Point[] points) {    // finds all line segments containing 4 or more points
         BruteCollinearPoints.validateInput(points);
-        lineSegments = new ArrayList<>();
+        endpoints = new HashMap<>();
+        lineSegments = new LinkedList<>();
 
-        for (int i = 0; i < points.length - 3; i++) {
-            points = sort(points);
+        Arrays.sort(points);
 
+        for (Point p : points) {
+            int count = 0;
+            int n = points.length - 1;
+            Point[] rest = new Point[n];
+            Arrays.sort(rest, p.slopeOrder());
+            for (int i = 0; i < n - 1; i++) {
+                if (Double.compare(p.slopeTo(rest[i]), p.slopeTo(rest[i + 1])) == 0) {
+                    count++;
+                } else {
+                    if (count > 2) {
+                        addSegment(p, rest[i]);
+                    }
+                    count = 0;
+                }
+            }
+            if (count > 2) {
+                addSegment(p, rest[n - 1]);
+            }
         }
     }
 
-    private Point[] sort(Point[] in) {
-        int n = in.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j > 0; j--) {
-                if (in[i].slopeOrder().compare(in[j], in[j + 1]) < 0) {
-                    Point swap = in[j];
-                    in[j] = in[j + 1];
-                    in[j + 1] = swap;
-                }
-            }
+    private void addSegment(Point p, Point that) {
+        Double slope = p.slopeTo(that);
+        LinkedList<Double> slopes = endpoints.getOrDefault(that, new LinkedList<>());
+        if (!slopes.contains(slope)) {
+            lineSegments.add(new LineSegment(p, that));
+            slopes.add(slope);
+            endpoints.put(that, slopes);
         }
-        return null;
     }
 
     public int numberOfSegments() {       // the number of line segments
